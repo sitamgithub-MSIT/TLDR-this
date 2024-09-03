f11a73f5d215d9de0db6e7b803ceb10bca43defd
sitammeur <sitammeur@gmail.com>
2024-09-03 22:28:24 +0000

    basic ui created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* <script src="./node_modules/preline/dist/preline.js"></script> */}
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="h-[50rem] w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+        <div className="relative overflow-hidden h-screen">
+          <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24">
+            <div className="text-center">
+              <h1 className="text-4xl sm:text-6xl font-bold text-neutral-200">
+                Anuvad <span className="text-[#f87315]">AI</span>
+              </h1>
+              <p className="mt-3 text-neutral-400">
+                <b>Anuvad AI:</b> Translate with ease, connect the world! 🌐✨
+              </p>
+              <div className="mt-7 sm:mt-12 mx-auto max-w-3xl relative">
+                <div className="grid gap-4 md:grid-cols-2 grid-cols-1">
+                  <div className="relative z-10 flex flex-col space-x-3 p-3 border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
