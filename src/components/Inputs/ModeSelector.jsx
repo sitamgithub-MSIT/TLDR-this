76a8beffa14d9ffcb3c5cc85861b3fa58f080472
sitammeur <sitammeur@gmail.com>
2024-09-12 04:54:51 +0000

    mode selection icon changed

--- src/components/Inputs/ModeSelector.jsx
+++ src/components/Inputs/ModeSelector.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { IconLanguage } from "@tabler/icons-react";
+import { IconDotsCircleHorizontal } from "@tabler/icons-react";
 
 const ModeSelector = ({ selectedMode, setSelectedMode, modes }) => (
   <span className="cursor-pointer rounded-full space-x-1 pl-2 bg-[#000000] flex items-center flex-row">
-    <IconLanguage size={20} />
+    <IconDotsCircleHorizontal size={20} />
     <select
       value={selectedMode}
       onChange={(e) => setSelectedMode(e.target.value)}
